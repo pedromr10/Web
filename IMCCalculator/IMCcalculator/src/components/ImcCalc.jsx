963bdaadf0cfe03d8973aade785587aaf9374b55
pedro <pedromunhozrosin@gmail.com>
2026-01-30 09:47:45 -0300

    adicionando projeto imc calc com react

---
+++ IMCCalculator/IMCcalculator/src/components/ImcCalc.jsx
@@ -0,0 +1,54 @@
+import Button from "./Button";
+import { useState } from "react";
+import "./ImcCalc.css";
+
+const ImcCalc = ({calcImc}) => {
+
+    const [height, setHeight] = useState("");
+    const [weight, setWeight] = useState("");
+
+    const clearForm = (e) =>{
+        e.preventDefault();
+        setHeight("");
+        setWeight("");
+    }
+
+    const validDigits = (text) =>{
+        return text.replace(/[^0-9,]/g, ""); //pega so numero e virugula
+    }
+
+    const handleHeightChange = (e) =>{
+        const updatedValue = validDigits(e.target.value);
+        setHeight(updatedValue);
+    }
+
+    const handleweightChange = (e) =>{
+        const updatedValue = validDigits(e.target.value);
+        setWeight(updatedValue);
+    }
+
+  return (
+    <div id="calc-container">
+        <h2>Calculadora de IMC</h2>
+        <form id="imc-form">
+            <div className="form-inputs">
+                <div className="form-control">
+                    <label htmlFor="height">Altura:</label>
+                    <input type="text" name="height" placeholder="Exemplo 1,75.." onChange={(e)=> handleHeightChange(e)} value={height}/>
+                </div>
+                <div className="form-control">
+                    <label htmlFor="weigth">Peso:</label>
+                    <input type="text" name="weigth" placeholder="Exemplo 70,5.." onChange={(e) => handleweightChange(e)} value={weight}/>
+                </div>
+            </div>
+            <div className="action-control">
+                <Button id="calc-btn" text="Calcular" action={(e) => calcImc(e, height, weight)}/>
+                <Button id="clear-btn" text="Limpar" action={clearForm}/>
+                
+            </div>
+        </form>
+    </div>
+  )
+}
+
+export default ImcCalc
